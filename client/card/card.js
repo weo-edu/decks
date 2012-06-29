04c52c3d36622469f1e9fb8b516e8280f8026381
danleavitt0 <daniel@daniel-GT5636E.(none)>
2012-06-29 14:00:48 -0700

    fixed error messages

--- client/card/card.js
+++ client/card/card.js
@@ -2,6 +2,7 @@ route('/card/create',function(ctx) {
 	Session.set('view','card_create');
 	var session = new _Session();
 	var card;
+	var last_success;
 	var view = ctx.view;
 	var rules = []
 	var error_message;
@@ -10,75 +11,79 @@ route('/card/create',function(ctx) {
 		'click #create' : function(){
 			console.log(session.get('card'));
 		},
-		'keyup .instant_update' : function(){
+		'focus .instant_update' : function(){
+			$(event.target.nextElementSibling).fadeIn();
+			$(event.target.nextElementSibling).css('background-color', 'green');
+			card = getValues();
+			testCard(false,event);
+		},
+		'keyup .instant_update' : function(event){
 			var success = true;
 			card = getValues();
-			try{
-				card = getCard();
-				$(".error").remove();
-			}
-			catch(err){
-				success = false;
-			}
-			if(success)
+			if(testCard(false, event))
+			{
+				last_success = getCard();
 				session.set('card', card);
+				$(event.target.nextElementSibling).css('background-color', 'green');
+			}
 		},
-		'focusout .instant_update' : function(){
+		'focusout .instant_update' : function(event){
+			//$(event.target.parentElement + " .error").remove();
+			$(event.target.nextElementSibling).fadeOut();
+			card = getValues();
 			if(!_.isEqual(session.get('card'), card))
 			{
-				try{
-					card = getCard();
-					session.set('card', card);
-					$(".error").remove();
-				}
-				catch(err)
-				{
-					var patt1 = / token: /
-					var patt2 = / end of input /
-					if(patt1.test(err.message))
-						error_message = "Invalid template";
-					else if(patt2.test(err.message))
-						error_message = "Invalid solution";
-					else
-						error_message = err.message;
-					console.log(error_message);
-					if(!$(event.target.parentElement).has('.error').length)
-					{
-						var error = "<label class='error'> *"+error_message+"</label>"
-						$(event.target).after(error);
-					}
-				}
+				testCard(true, event);
 			}
-			session.set('card', card);
+			//session.set('card', card);
 		},
 		'keyup #rules' : function(event){
+			removeError($("#rules"));
+			var success = true;
+			try{
+				rules.push($('#rules').val());
+				card = getCard();
+			}
+			catch(err)
+			{
+				success = false;
+				rules.pop();
+				card = getCard();
+				$(event.target.nextElementSibling).css('background-color', 'red');
+			}
+			if(success)
+			{
+				$(event.target.nextElementSibling).css('background-color', 'green');
+				rules.pop();
+			}
 			if(event.keyCode == 13)
 			{	
-				var success = true;
-				try{
-					rules.push($('#rules').val());
-					card = getCard();
-				}
-				catch(err)
+				if(success)
 				{
-					success = false;
-					rules.pop();
-					card = getCard();
+					rules.push($('#rules').val());
+					session.set('card', card);
+					$('#rules_list').append('<li>'+rules[rules.length-1]+'<span class="remove">-</span></li>');
+					$("#rules").val('');
+					$("#rules").focus();
+					console.log(session.get('card'));
+				}
+				else{
 					error_message = "Invalid rule";
 					if(!$(event.target.parentElement).has('.error').length)
 					{
-						var error = "<label class='error'> *"+error_message+"</label>"
-						$(event.target).after(error);
+						var error = "<label class='error'>"+error_message+"</label>"
+						$(error).insertAfter($(event.target.nextElementSibling));
 					}
 				}
-				if(success)
-				{
-					session.set('card', card);
-					$('#rules_list').append('<li>'+$("#rules").val()+'<span class="remove">-</span></li>');
-					$("#rules").val('');
-					$("#rules").focus();
-				}
 			}
+		},
+		'focusout #rules' : function(event) {
+			$(event.target.nextElementSibling).fadeOut();
+		},
+		'focus #rules' : function(event)
+		{
+			$(event.target.nextElementSibling).css('background-color', 'green')
+			$(event.target.nextElementSibling).fadeIn();
 		},
 		'click .remove' : function (event)
 		{
@@ -97,6 +102,117 @@ route('/card/create',function(ctx) {
 
 	Template.card_create.events = events;
 
+	function testCard(for_color, event){
+		try{
+			card = getCard();
+			session.set('card', card);
+			$(".error").remove();
+			return true;
+		}
+		catch(err)
+		{
+			errorSwitch(err, for_color, event)
+			return false;
+		}
+	}
+	function errorSwitch(err, for_color, event)
+	{
+		switch(errorAssign(err, for_color))
+		{
+			case "template":
+				try
+				{
+					if($(event.target).is($('#template')))
+						$(event.target).next().css('background-color', 'red');
+					card = getValues();
+					card.template = last_success.template;
+					getCard(card);
+					removeError($('#solution'));
+				}
+				catch(err)
+				{
+					if($(event.target).is($('#solution')))
+						$(event.target).next().css('background-color', 'red');
+					errorAssign(err, for_color);
+				}
+			break;
+			case "solution":
+				try
+				{
+					if($(event.target).is($('#solution')))
+						$(event.target).next().css('background-color', 'red');
+					card = getValues();
+					card.solution = last_success.solution;
+					getCard(card);
+					removeError($('#template'));
+				}
+				catch(err)
+				{
+					if($(event.target).is($('#template')))
+						$(event.target).next().css('background-color', 'red');
+					errorAssign(err,for_color);
+					//$('#template').next().css('background-color', 'red');
+				}
+			break;
+			default:
+			break;
+		}
+	}
+	function errorMessage(target, message)
+	{
+		var tar = target.parent();
+		if(!$(tar).has('.error').length)
+		{
+			var error = "<label class='error'>"+error_message+"</label>"
+			$(tar).append(error);
+		}
+	}
+	function removeError(target)
+	{
+		var tar = target.parent();
+		var tar_ball = target.next();
+		if(tar.has($('.error')))
+		{
+			tar.children('.error').remove();
+		}
+		$(tar_ball).css('background-color', 'green');
+	}
+	function errorAssign(error, send_error)
+	{
+		var error_messagel;
+		var err = error;
+		var patt1 = / token: /
+		var patt2 = /end of input/
+		var patt3 = /not defined/
+		var patt4 = /of undefined/
+		if(patt1.test(err.message))
+			{
+				if(send_error)
+				{
+					error_message = "Invalid template";
+					errorMessage($("#template"), error_message);
+				}
+				return "template";
+			}
+			else if(patt2.test(err.message) || patt3.test(err.message) || patt4.test(err.message))
+			{
+				if(send_error)
+				{
+					error_message = "Invalid solution";
+					errorMessage($("#solution"), error_message);
+				}
+				return "solution";
+			}
+			else
+			{
+				if(send_error)
+				{
+					error_message = err.message;
+					errorMessage($(event.target), error_message);
+				}
+				return "else";
+			}
+	}
 	function getValues(){
 		var update_card = {};
 		update_card.name = $('#card_name').val();
@@ -106,8 +222,11 @@ route('/card/create',function(ctx) {
 		update_card.rules = rules;
 		return update_card;
 	}
-	function getCard(){
-		var update_card = getValues();
+	function getCard(fix_card){
+		if(fix_card)
+			var update_card = fix_card;
+		else
+			var update_card = getValues();
 		update_card.problem = new Problem(update_card);
 		update_card.question = update_card.problem.generate();
 		update_card.answer = update_card.problem.getSolution();
@@ -117,5 +236,4 @@ route('/card/create',function(ctx) {
 	Template.card_play.card = function(){
 		return session.get('card');
 	}
-
 });
