d2cafd269f807f831f9bfb24c81338d257ee1fa5
joshrtay <joshrtay@gmail.com>
2012-07-03 11:52:20 -0700

    card refractor

--- client/card/card.js
+++ client/card/card.js
@@ -1,88 +1,113 @@
 route('/card/create',function(ctx) {
-
+	console.log('card create');
 	var session = new _Session();
-	var card = new _Session();
-	card.set('template','');
-	card.set('solution','');
-	var error = new _Session();
+	var card = new _Session({
+		template: '',
+		solution: '',
+		graphic: null,
+		rules: []
+	});
+	var error = new _Session({
+		template: '',
+		solution: '',
+		rules: []
+	});
 
 	var last_success;
 	var rules = []
 	var error_message;
 
-	var state = {FIELD: null}
-
-	var events = {
-		'focus .instant_update' : function(){
-			var id = $(event.target).attr('id');
-			$('#'+id+'-light').fadeIn();
-
-		},
-		'keyup .instant_update' : function(event){
-			var el = $(event.target);
-			var id = $(event.target).attr('id');
-			var val = el.val();
-			state.FIELD = id;
-
-			if (!card.equals(id,val))
-				card.set(id,val);
-		},
-		'focusout .instant_update' : function(event){
-			//$(event.target.parentElement + " .error").remove();
-			var id = $(event.target).attr('id');
-			$('#'+id+'-light').fadeOut();
-
-		},
-		'keyup #rules' : function(event){
-			event.stopPropogation();
-
-		
-		},
-		'focusout #rules' : function(event) {
-			$(event.target.nextElementSibling).fadeOut();
-		},
-		'focus #rules' : function(event)
-		{
-			$(event.target.nextElementSibling).css('background-color', 'green')
-			$(event.target.nextElementSibling).fadeIn();
-		},
-		'click .remove' : function (event)
-		{
-
-		},
-		'insert .create' : function(evt){
-	    $('#file').fileupload({
+	Meteor.defer(function() {
+		console.log('file', $('#file'));
+		$('#file').fileupload({
 	    	url: "/upload",
 	    	type: "POST",
 	    	dataType: 'json',
 	    	multipart: true,
 	    	done: function(e,data) {
+	    		console.log('done');
 	    		card.set("graphic","/upload/"+data.result.path);
 		    }
 	    });
+	})
+
+	var events = {
+		'keyup .instant_update' : function(event){
+			var el = $(event.target);
+			var id = $(event.target).attr('id');
+			var val = el.val();
+
+			if (!card.equals(id,val))
+				card.set(id,val);
 		}
 	};
 
 	Template.card_create.events = events;
 
+
+	Template.rules.events = {
+		'keyup .instant_update': function(event) {
+			var el = $(event.target);
+			var idx = el.parent().children(".rule-input").index(el);
+			console.log(idx);
+			var rules = _.clone(card.get('rules'));
+			console.log('rules',rules);
+			rules[idx] = el.val();
+			card.set('rules',rules);
+			return false;
+		},
+		'click #add-rule': function() {
+			$('#rules').prepend(Meteor.ui.render(function() {
+				return Template.rule_input();
+			}));
+			var rules = card.get('rules');
+			rules.unshift('');
+			card.set('rules',rules);
+		}
+	}
 	
 	Template.card_play.card = function(){
 		var c = card.all();
-		console.log(c);
-		try {
-			c.problem = new Problem(c);
-			c.question = c.problem.generate();
-			c.answer = c.problem.getSolution();
-		} catch (e) {
-			console.log(e);
-		}
-		
+		console.log('c',c);
+		var p = problemize(c);
+		c.question = p.html;
+		c.answer = p.solution;
+		var e = {
+			template: '',
+			solution: '',
+			rules: _.map(c.rules,function(rule) {return '';})
+		};
+
+		console.log('e',e);
+
+		_.each(p.errors,function(err) {
+			console.log(err.stack);
+			if (err.part == 'rule') {
+				console.log('rule error',err.idx);
+				e.rules[err.idx] = err.message;
+			}
+				
+			else
+				e[err.part] = err.message;
+		});
+
+		_.each(e,function(val,key) {
+			error.set(key,val);
+		})
+
  
 		return c;
 	}
 
-	Template.card_create.error = function() {
-		return error.all();
+	Template.template_errors.error = function() {
+		return error.get('template');
+	}
+
+	Template.solution_errors.error = function() {
+		return error.get('solution');
+	}
+	Template.rules_errors.rules_errors = function() {
+		return error.get('rules');
 	}
 	renderView('card_create');
 });
