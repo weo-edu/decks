71093a9eeeb5e86f70df5c6cd506b8be5b9dbb93
Daniel Leavitt <danleavitt0@gmail.com>
2012-07-12 10:31:14 -0700

    re-styled card creator

--- client/card/card.js
+++ client/card/card.js
@@ -1,4 +1,5 @@
-route('/card/create',function(ctx) {
+route('/card/create', function() {
+	var transformPrefix = domToCss(Modernizr.prefixed('transform'));
 	console.log('card create');
 	var session = new _Session();
 	var card = new _Session({
@@ -14,23 +15,39 @@ route('/card/create',function(ctx) {
 		rules: []
 	});
 
-	var last_success;
-	var rules = []
-	var error_message;
 
-	Meteor.defer(function() {
-		console.log('file', $('#file'));
-		$('#file').fileupload({
-	    	url: "/upload",
-	    	type: "POST",
-	    	dataType: 'json',
-	    	multipart: true,
-	    	done: function(e,data) {
-	    		console.log('done');
-	    		card.set("graphic","/upload/"+data.result.path);
-		    }
-	    });
-	})
+
+	var watchErrors = function(){
+		var update = function(){
+			Meteor.defer(function(){
+				var err = error.all();
+				_.each(err, function(msg, ele){
+					if(ele == 'rules')
+					{
+						_.each(msg, function(mssg, elem){
+							var num = $('#rules').children().index() - elem;
+							var el = '#rules-' + num.toString();
+							if(mssg)
+								$(el).addClass('error');
+							else
+								$(el).removeClass('error');
+						})
+					}
+					else
+					{
+						var el = '#' + ele;
+						if(msg)
+							$(el).addClass('error');
+						else
+							$(el).removeClass('error');
+						console.log(msg);
+					}
+					//console.log(test);	
+				}); 
+			});
+		};
+		update();
+	}
 
 	var events = {
 		'keyup .instant_update' : function(event){
@@ -39,14 +56,30 @@ route('/card/create',function(ctx) {
 			var val = el.val();
 
 			if (!card.equals(id,val))
+			{
 				card.set(id,val);
+				watchErrors(el, id);
+			}
 		},
 		'click #create' : function(){
 			var c = card.all()
 			Cards.insert(c, function(){
 				console.log(Cards.findOne(c));
 			});
-			animateCreator();
+			animateCreator(function(){
+			});
+		},
+		'mouseover .error' : function(){
+			var el = $(event.target);
+			var id = $(event.target).attr('id');
+			err_msg = el.parent().children('.error-message');
+			console.log(err_msg);
+		},
+		'click .color-picker-min' : function(){
+			$('.color-choose').toggle();
+		},
+		'click .color-choose' : function() {
+			$('.color-choose').toggle();
 		}
 	};
 
@@ -56,13 +89,12 @@ route('/card/create',function(ctx) {
 	Template.rules.events = {
 		'keyup .instant_update': function(event) {
 			var el = $(event.target);
-			//var idx = el.parent().children(".rule-input").index(el);
 			var idx = el.parents('#rules').children().children('.rule-input').index(el);
-			console.log(idx);
 			var rules = _.clone(card.get('rules'));
-			console.log('rules',rules);
+
 			rules[idx] = el.val();
 			card.set('rules',rules);
+			watchErrors();
 			return false;
 		},
 		'click #add-rule': function() {
@@ -74,39 +106,29 @@ route('/card/create',function(ctx) {
 			card.set('rules',rules);
 		}
 	}
-	
-	Template.template_errors.events = {
-		'mouseover .error-light-red' : function (event) {
-			$('#template-error-message').css('display','inline-block');
-		},
-		'mouseout .error-light-red' : function() {
-			$('#template-error-message').fadeOut('fast');
-		}
-	}
 
-	Template.solution_errors.events = {
-		'mouseover .error-light-red' : function (event) {
-			$('#solution-error-message').css('display','inline-block');
-		},
-		'mouseout .error-light-red' : function() {
-			$('#solution-error-message').fadeOut('fast');
-		}
-	}
-
-	Template.rules_errors.events = {
-		'mouseover .error-light-red' : function (event) {
-			var el = $(event.target).parent().children('#rules-error-message');
-			var light = $(event.target).parent().children('.error-light-red');
-			var cssObj = {
-				'display': 'block',
-				'top': $(light).position().top,
-				'left': $(light).position().left + $(light).width()
+	Template.deck_preview.events = {
+		'click .deck' : function(event){
+			el = $(event.target).closest('.deck-container');
+			el.removeClass('last-selected')
+			el.toggleClass('selected');
+			if(el.hasClass('selected'))
+			{
+				el.parent().children().not('.selected').hide();
+				el.css(transformPrefix, 'rotateY(180deg)');
 			}
-			$(el).css(cssObj);
+			else
+			{ 
+				el.addClass('last-selected');
+				deal($('.deck-preview'),200, 'grid');
+				el.css(transformPrefix, 'rotateY(0deg)');
+				el.parent().children().fadeIn(500);
+			}
+			return false;
 		},
-		'mouseout .error-light-red' : function() {
-			var el = $(event.target).parent().children('#rules-error-message');
-			$(el).fadeOut('fast');
+		'click .choose-deck' : function(event){
+			el = $(event.target).closest('.deck-container');
+			el.toggleClass('chosen');
 		}
 	}
 
@@ -143,10 +165,6 @@ route('/card/create',function(ctx) {
 		return c;
 	}
 
-	Template.deck_list.deck = function(){
-		return Decks.find({});
-	}
-
 	Template.template_errors.error = function() {
 		return error.get('template');
 	}
@@ -154,15 +172,39 @@ route('/card/create',function(ctx) {
 	Template.solution_errors.error = function() {
 		return error.get('solution');
 	}
-	Template.rules_errors.rules_errors = function() {
-		console.log(error.get('rules'));
-		return error.get('rules');
+
+	Template.rule_input.idx = function(){
+		return $('#rules').children().length;
 	}
+	Template.deck_preview.deck = function(){
+		Meteor.defer(function() {
+			$('#colorpicker').farbtastic('#color');
+			console.log('file', $('#file'));
+			$('#file').fileupload({
+		    	url: "/upload",
+		    	type: "POST",
+		    	dataType: 'json',
+		    	multipart: true,
+		    	done: function(e,data) {
+		    		console.log('done');
+		    		card.set("graphic","/upload/"+data.result.path);
+			    }
+		    });
+			});
+		return Decks.find({});
+	}
+
 	function animateCreator(){
-		var pt1 = $('#template-preview').position().left;
-		var pt2 = $('#card-preview').position().left;
-		var slide = pt2-pt1;
-		$('.display').animate({'left': '-='+slide});
+		//var pt1 = $('#template-preview').position().left;
+		//var pt2 = $('#card-preview').position().left;
+		var slide = 0;
+		$('.display').animate({'left': '-='+slide}, function(){
+			$('#deck-preview').show();
+		});
+		deal($('.deck-preview'),600,'grid');
+		$('#template-preview').fadeOut();
+		$('.color-picker-min').fadeOut();
 	}
+	//watchErrors();
 	renderView('card_create');
 });
