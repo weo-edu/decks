8dbaaf0401743d6e7da18a6c6d03453195e28553
mweint <mweint@gmail.com>
2012-07-12 04:40:13 -0700

    reworked deck/play new animationInsert function (still buggy)

--- client/deck/deck.js
+++ client/deck/deck.js
@@ -70,48 +70,53 @@ route('/deck/play/:name', function(ctx){
 			cur_card.question = problems[i].html;
 		}
 
-		if(deck)
-			Meteor.defer(initDecks);
-
 	  	return deck;
   	}
-
-  	var initDecks = _.once(function() {
-  		var answered = $('#answered');
-		var unanswered = $('#unanswered');
-
-		answered.width(answered.children().width());
-		unanswered.width(unanswered.parent().width() - answered.width() - 20);
-
-		deal($('#deck-dock'), 0);
-		featureCard(unanswered.children().eq(0), 0);
-		
-		$("#playground").slideDown(1000, function(){
-				$('#playground .solution').focus();
-				// $('#unanswered .card').eq(0).click();
-		});
-  	});
 
 
 
 
   	Template.deck_play.events = {
+  		'render': function() {
+  			var answered = $('#answered');
+			var unanswered = $('#unanswered');
 
+			answered.width(answered.children().width());
+			unanswered.width(unanswered.parent().width() - answered.width() - 20);
+
+			deal($('#deck-dock'), 0);
+			featureCard(unanswered.children().eq(0), 0);
+			
+			$("#playground").slideDown(1000, function(){
+					$('#playground .solution').focus();
+					$('#unanswered .card').eq(0).click();
+			});
+  		},
   		'click #unanswered .card': function(e) {
 			$('.card-container').removeClass('current');
-	  		var el = $(e.target).closest('.card-container').addClass('current');
+	  		var el = $(e.target).closest('.card-container');
 	  		working_card = (el.attr('data') - 1);
 	  		MathJax.Hub.Queue(["Typeset", MathJax.Hub, el.find('.question').get(0)]);
 	  		// $('#play-card').html(el.find('.back').html());
-	  		if($('#playground .card-container').index() != -1)
-  				$('#playground .card-container').animateInsert('append', $('#unanswered'));
 
+
+	  		var lastChild = $('#unanswered .card-container:last-child');
+	  		var values = lastChild.css(transformPrefix).split('(')[1].split(')')[0].split(',');
+	  		var skipCoords = {};
+	  		skipCoords.top = lastChild.offset().top;
+	  		skipCoords.left = lastChild.offset().left;
+	  		skipCoords.y = -Math.round(Math.asin(values[2]) * (180/Math.PI));	  		 
+
+	  		if($('#playground .card-container').index() != -1)
+  				$('#playground .card-container').animateInsert('append', $('#unanswered'), function(){}, skipCoords);
+  					
 	  		el.animateInsert('prepend', $('#playground'), function(){
 	  			$('#playground .current .solution').focus();
-	  		});
+	  		}).addClass('current');
+	  		
+	  		el.addClass('current');
 
 	  		featureCard($('#unanswered .card-container').eq(0));
-	  		
 	  	},
 	  	'mouseenter #unanswered .card-container': function(e) {
 	  		var el = $(e.currentTarget);
@@ -121,29 +126,24 @@ route('/deck/play/:name', function(ctx){
 	  		featureCard($(e.currentTarget).find('.current'));
 	  	},
 	  	'keydown #playground .solution': function(e){
-	  		var cur_idx = $('#unanswered .card-container.current').index();
+	  		// var cur_idx = $('#unanswered .card-container.current').index();
 
 	  		if(e.which === 13)
 	  		{
 	  			el = $('#playground .card-container');
 
 	  			el.animateInsert('prepend', $('#answered'));
-	  			el.removeClass('correct wrong');
+	  			// el.removeClass('correct wrong');
 
 	  			var result = e.target.value == problems[working_card].solution;	
-
-	  			// console.log(e.target.value, problems[working_card].solution);
 	  			results.push(result);
-	  			// problems.splice(working_card, 1);
 	  			problems[working_card].answered = 1;
 
-	  			if(result)
-	  				el.addClass('correct');	  				
-	  			else
-	  				el.addClass('wrong');
+	  			// if(result)
+	  			// 	el.addClass('correct');	  				
+	  			// else
+	  			// 	el.addClass('wrong');
 
-
-	  			// deal($('#unanswered'), 0, 'fit', function(){
   				$('#unanswered .card').eq(0).click();
 	  			$('#playground .solution').focus();
 	  			deal($('#answered'), 100, 'collapse');
@@ -154,18 +154,18 @@ route('/deck/play/:name', function(ctx){
 		  						count++;
 
 		  				}
-						renderView('deck_results');
+	  					renderView('deck_results');	
+						
 		  		}
 	  			
 	  		}
-	  		else if(e.which === 37 && cur_idx != 0)
-	  			$('#unanswered .card-container .card').eq(cur_idx - 1).click();
+	  		else if(e.which === 37)
+	  			$('#unanswered .card-container:last-child .card').click();
 	  		else if(e.which === 39)
-	  			$('#unanswered .card-container .card').eq(cur_idx + 1).click();
+	  			$('#unanswered .card-container .card').eq(0).click();
 
 	  		$('#playground .solution').focus();	
 	  	}
-
   	}
 
   	Template.deck_results.correct = function(){
@@ -179,42 +179,31 @@ route('/deck/play/:name', function(ctx){
 	renderView('deck_play');
 
 	$.fn.extend({
-	    animateInsert: function(type, container, callback){
+	    animateInsert: function(type, container, callback, endCoords){
 	    	callback = callback || function(){};
 			var offset = this.offset();
 
-			offset.left -= parseInt(matrixToArray(this.css(transformPrefix))[4], 10);
-			offset.top -= parseInt(matrixToArray(this.css(transformPrefix))[5], 10);
-
-			// console.log(matrix);
-
-
-			var stage = $('<div id="stage" style="position: absolute; height: 100%; width: 100%; z-index: 9999;"></div>');
+			var stage = $('<div class="stage" style="position: absolute; height: 100%; width: 100%; z-index: 9999;"></div>');
 
 	    	$('body').prepend(stage);
 
-	    	if(type == 'prepend')
-	    		var new_el = this.clone().css('visibility', 'hidden').prependTo(container);
-	    	else if(type == 'append')
-	    		var new_el = this.clone().css('visibility', 'hidden').appendTo(container);
+	    	var new_el = this.clone().css(transformPrefix, 'translate3d(0,0,0)').css('visibility', 'hidden');
+	    	(container)[type](new_el);
 
-	    	// this.css(transformPrefix, matrix);
-	    	this.prependTo(stage).css({'position': 'absolute', 'top': offset.top, 'left': offset.left});
+	    	this.css({'position': 'absolute'}).css(transformPrefix, 'translate3d(' + offset.left + 'px, ' + offset.top + 'px, 0)').prependTo(stage);
 
-	    	// console.log('origin', offset);
-
-
-	    	var new_offset = new_el.offset();
-
-
-	    	
-	    	var new_matrix = new_el.css(transformPrefix);
+	    	var new_offset = new_el.offset();	    	
 	    	
 	    	var that = this;
-    		setTimeout(function(){
-    			that.css(transformPrefix, new_matrix);
-    			that.css({'top': new_offset.top, 'left': new_offset.left});
-    			console.log(new_offset);
+    		setTimeout(function(){    			
+    			if(endCoords)
+    				new_offset = endCoords;
+
+    			var rotateY = new_offset.y ? new_offset.y : 0;
+
+    			console.log(rotateY);
+
+				that.css(transformPrefix, 'translate3d(' + new_offset.left + 'px, ' + new_offset.top + 'px, 0) rotateY('+ rotateY + 'deg)');
     		}, 0);
 
     		this.get(0).addEventListener( 
@@ -225,12 +214,9 @@ route('/deck/play/:name', function(ctx){
     				callback(); 		
 	     		}, false);
 
-	    	function matrixToArray(matrix) {
-			    return matrix.substr(7, matrix.length - 8).split(', ');
-			}
+    		return new_el;
 	    }
 	});
-
 });
 
 
