e2c355b8755c9aa2144952a46b3ff94069b09e50
Andrew Shaffer <darawk@gmail.com>
2012-07-12 02:45:34 +0000

    added template render event

--- client/weo.js
+++ client/weo.js
@@ -9,14 +9,22 @@ Meteor.startup(function() {
 
 
 function renderView(template) {
+	if(!Template[template])
+		throw new Error('Invalid template name');
+
+	var tmpl = Template[template];
 	Meteor.defer(function(){
 		$('#content').html(function(){
 			return Meteor.ui.render(function(){
 				//	Force all templates to be wrapped in an blank div
 				//	This fixes a bug(?) in Meteor's liverange code
-				return '<div>' + Template[template]() + '</div>';
+				return '<div>' + tmpl() + '</div>';
 			});
 		});
+
+		if(tmpl.events['render']){
+			tmpl.events['render']();
+		}
 	});
 }
 
