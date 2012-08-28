6df067787876c4c4b66e43f9545fa882421e99bc
mweint <mweint@gmail.com>
2012-08-28 15:41:06 -0700

    progress bars animate

--- server/weo.js
+++ server/weo.js
@@ -28,7 +28,7 @@ app.get('/upload/*',express.static(process.cwd()+"/.meteor/"));
 var cards = [
                 {
                         name: '1\'s',
-                        graphic: 'decks/simple-sums/1.png',
+                        graphic: '/decks/simple-sums/1.png',
                         problem: {
                                 template: '1 + {{a}}  = ?',
                                 solution: '1 + a'
@@ -36,7 +36,7 @@ var cards = [
                 },
                 {
                         name: '2\'s',
-                        graphic: 'decks/simple-sums/2.png',
+                        graphic: '/decks/simple-sums/2.png',
                         problem: {
                                 template: '2 + {{a}}  = ?',
                                 solution: '2 + a'
@@ -44,7 +44,7 @@ var cards = [
                 },
                 {
                         name: '3\'s',
-                        graphic: 'decks/simple-sums/3.png',
+                        graphic: '/decks/simple-sums/3.png',
                         problem: {
                                 template: '3 + {{a}}  = ?',
                                 solution: '3 + a'
@@ -52,7 +52,7 @@ var cards = [
                 },
                 {
                         name: '4\'s',
-                        graphic: 'decks/simple-sums/4.png',
+                        graphic: '/decks/simple-sums/4.png',
                         problem: {
                                 template: '4 + {{a}}  = ?',
                                 solution: '4 + a'
@@ -60,7 +60,7 @@ var cards = [
                 },
                 {
                         name: '5\'s',
-                        graphic: 'addition.gif',
+                        graphic: '/decks/simple-sums/5.png',
                         problem: {
                                 template: '5 + {{a}}  = ?',
                                 solution: '5 + a'
@@ -68,7 +68,7 @@ var cards = [
                 },
                 {
                         name: '6\'s',
-                        graphic: 'addition.gif',
+                        graphic: '/decks/simple-sums/6.png',
                         problem: {
                                 template: '6 + {{a}}  = ?',
                                 solution: '6 + a'
@@ -76,7 +76,7 @@ var cards = [
                 },
                 {
                         name: '7\'s',
-                        graphic: 'addition.gif',
+                        graphic: '/decks/simple-sums/7.png',
                         problem: {
                                 template: '7 + {{a}}  = ?',
                                 solution: '7 + a'
@@ -84,7 +84,7 @@ var cards = [
                 },
                 {
                         name: '8\'s',
-                        graphic: 'addition.gif',
+                        graphic: '/decks/simple-sums/8.png',
                         problem: {
                                 template: '8 + {{a}}  = ?',
                                 solution: '8 + a'
@@ -92,7 +92,7 @@ var cards = [
                 },
                 {
                         name: '9\'s',
-                        graphic: 'addition.gif',
+                        graphic: '/decks/simple-sums/9.png',
                         problem: {
                                 template: '9 + {{a}}  = ?',
                                 solution: '9 + a'
@@ -103,7 +103,15 @@ var cards = [
     var decks = [
         {
             title: 'Simple Sums',
-            image: 'decks/simple-sums/simple-sums.png',
+            image: '/decks/simple-sums/simple-sums.png',
+            colorScheme: {
+                primary: 'skyblue',
+                secondary: 'green'
+            }
+        },
+        {
+            title: 'Mini Minus',
+            image: '/decks/mini-minus/mini-minus.png',
             colorScheme: {
                 primary: 'skyblue',
                 secondary: 'green'
