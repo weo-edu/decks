48739ea1a2f91989e2f556685d4d4cd356af2d03
joshrtay <joshrtay@gmail.com>
2012-10-17 16:35:08 -0700

    filters

--- client/goat-guru/browse/browse.js
+++ client/goat-guru/browse/browse.js
@@ -20,7 +20,75 @@ route('/goat',function() {
 
 });
 
+var toggle = {};
+
+
+toggle.deckFilter = function(routeSession, userId) {
+	if (routeSession.get('toggle') === 'collected') {
+		var query = {user: userId};
+		query['mastery.rank'] = {$gt: 0};
+		var deckInfos = UserDeckInfo.find(query, {sort: {last_played: -1}}).fetch();
+		_.each(deckInfos, function(deckInfo) {
+			_.extend(deckInfo, Decks.findOne(deckInfo.deck)); 
+		});
+	} else if (routeSession.get('toggle') === 'played') {
+		var query = {user: userId};
+		var deckInfos = UserDeckInfo.find(query, {sort: {last_played: -1}}).fetch();
+		_.each(deckInfos, function(deckInfo) {
+			_.extend(deckInfo, Decks.findOne(deckInfo.deck)); 
+		});
+	} else if (routeSession.get('toggle') === 'created') {
+		var deckInfos = Decks.find({creator: userId}).fetch();
+		_.each(deckInfos, function(deckInfo) {
+			_.extend(deckInfo, UserDeckInfo.findOne(deckInfo._id));
+		});
+	}
+
+
+	//XXX this should return a cursor
+
+	return deckInfos;
+}
+
+toggle.helpers = function(routeSession) {
+	return {
+		collectedToggle: function() {
+			return routeSession.equals('toggle', 'collected') ? 'active' : '';
+		},
+		createdToggle: function() {
+			return routeSession.equals('toggle', 'created') ? 'active' : '';
+		},
+		playedToggle: function() {
+			return routeSession.equals('toggle', 'played') ? 'active' : '';
+		}
+	}
+}
+
+toggle.events = function(routeSession) {
+	return {
+		'click #collected-toggle': function() {
+			routeSession.set('toggle', 'collected');
+		},
+		'click #created-toggle': function() {
+			routeSession.set('toggle', 'created');
+		},
+		'click #played-toggle': function() {
+			routeSession.set('toggle', 'played');
+		}
+	}
+}
+
 route('/friends',function() { 
+	/*console.log('user', Session.get('active'));
+	var decks = join({
+		cursor: [
+			UserDeckInfo.find({user: {$in: Meteor.user().friends}}),
+			Decks.find({})
+		],
+		on: ['decks._id', 'UserDeckInfo.deck']
+	});
+
+	console.log('decks',decks.find({}).fetch())*/
 
 	Template.tome.events({
 		'click': function() {
@@ -30,12 +98,10 @@ route('/friends',function() {
 		}
 	});
 
-	Template.friends_browse.rendered = function() {
-		console.log('rendered friend browse');
-	}
 	Template.buddies.events({
 		'click .buddy': function(e) {
 			Session.set('active', this);
+			routeSession.set('toggle', 'collected');
 		},
 	});
 
@@ -62,23 +128,27 @@ route('/friends',function() {
 	});
 
 	Template.opponent_decks.helpers({
-		'tomes': function() {
-			var active = Session.get('active');
-			if(active) {
-				Meteor.subscribe('UserDeckInfo', active._id);
-
-				var deckInfos = UserDeckInfo.find({user: active._id}).fetch();
-				_.each(deckInfos, function(i) {
-					_.extend(i, Decks.findOne(i.deck)); 
-				});
-				return deckInfos;
-			}
-		},
 		'user': function() {
 			var active = Session.get('active');
 			return active ? active : false;
 		}
 	});
+
+	Template.browse_tomes.helpers({
+		'tomes': function() {
+			var active = Session.get('active');
+			if(active) {
+				Meteor.subscribe('UserDeckInfo', active._id);
+				return toggle.deckFilter(routeSession, active._id);
+			}
+		}
+	});
+
+	Template.browse_filters.helpers(toggle.helpers(routeSession));
+
+	Template.browse_filters.events(toggle.events(routeSession));
+
+	routeSession.set('toggle', 'collected');
 
 	dojo.render('friends_browse');
 });
@@ -93,17 +163,18 @@ route('/inventory', function() {
 		}
 	});
 
-	Template.my_collection.helpers({
+	Template.browse_tomes.helpers({
 		tomes: function() {
-			var deckInfos = UserDeckInfo.find({user: Meteor.user()._id}).fetch();
-			
-			_.each(deckInfos, function(i) {
-				_.extend(i, Decks.findOne(i.deck)); 
-			});
-			
-			return deckInfos; 
+			return toggle.deckFilter(routeSession, Meteor.user()._id);
 		}
 	});
 
+	Template.browse_filters.helpers(toggle.helpers(routeSession));
+
+	Template.browse_filters.events(toggle.events(routeSession));
+
+	routeSession.set('toggle', 'collected');
+
 	dojo.render('my_collection');
+
 });
